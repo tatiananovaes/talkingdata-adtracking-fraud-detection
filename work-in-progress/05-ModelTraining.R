1cdc6b2df2cf03a77cf5e8e3c09cf9791d2742e0
Tatiana Novaes <tatiananovaescarvalho@gmail.com>
2022-02-09 01:16:51 -0300

    Finalização da modelagem.

--- work-in-progress/05-ModelTraining.R
+++ work-in-progress/05-ModelTraining.R
@@ -10,14 +10,12 @@
 
 library(data.table)
 library(parallel)
-library(class) # para modelo knn
-library(e1071) # para modelo svm
-library(rpart) # para modelo Random Forest
+library(rpart)
 require(xgboost)
 library(caret)
 library(pROC)
 library(ROCR)
-library(gmodels) # para avaliar modelo com CrossTable
+library(gmodels)
 
 
 ############# Funções Auxiliares // Functions #################
@@ -47,7 +45,14 @@ del_column <- function(dt, feat) {
   dt <- dt[, (feat) := NULL]
 }
 
-
+metricas <- function(model, pred, nome){
+  results <- caret::confusionMatrix(table(data = pred, reference = test_target))
+  acuracia <- results$overall['Accuracy']
+  kappa <- results$overall['Kappa']
+  curva_roc <- multiclass.roc(response = test_target, predictor = as.numeric(as.factor(pred)))
+  auc <- curva_roc$auc
+  return(c(nome, round(acuracia, 4), round(kappa, 4), round(auc, 4)))
+}
 
 ########## Carga de dados // Reading data files ##########
 
@@ -78,6 +83,8 @@ summary(test_data)
 
 ########## Treinando os modelos // Model training ##############
 
+set.seed(1234)
+
 formula_model_a <- as.formula("is_attributed ~ .")
 formula_model_b <- as.formula("is_attributed ~ hour_click + app + diff_time_click + channel + count_click_by_ip") # Método MeanDecreaseAccuracy
 formula_model_c <- as.formula("is_attributed ~ app + diff_time_click + channel + count_click_by_ip + device") # Método MeanDecreaseGini
@@ -85,14 +92,6 @@ formula_model_d <- as.formula("is_attributed ~ app + channel + device + os + dif
 
 test_target <- test_data$is_attributed
 
-metricas <- function(model, pred, nome){
-  results <- caret::confusionMatrix(table(data = pred, reference = test_target))
-  acuracia <- results$overall['Accuracy']
-  kappa <- results$overall['Kappa']
-  curva_roc <- multiclass.roc(response = test_target, predictor = as.numeric(as.factor(pred)))
-  auc <- curva_roc$auc
-  return(c(nome, round(acuracia, 4), round(kappa, 4), round(auc, 4)))
-}
 
 
 # Versão 1 - Regressão Logística com todas as variáveis
@@ -107,7 +106,7 @@ print(vetor_model_glm1)
 saveRDS(model_glm1, 'models/model_glm1.rds')
 
 # day_of_year_click = NA (sem significância)
-# acuracia 0.7201 kappa 0.0091 auc 0.7604
+# acuracia 0.7207 kappa 0.0091 auc 0.7607
 
 
 # Versão 2 - Regressão Logística com variáveis selecionadas com Random Forest (Método MeanDecreaseAccuracy)
@@ -122,7 +121,7 @@ print(vetor_model_glm2)
 saveRDS(model_glm2, 'models/model_glm2.rds')
 
 # todas as variáveis com ***
-# acuracia 0.7200 kappa 0.0091 auc 0.7600
+# acuracia 0.7205 kappa 0.0091 auc 0.7603
 
 
 # Versão 3 - Regressão Logística com variáveis selecionadas (Método MeanDecreaseGini)
@@ -137,7 +136,7 @@ print(vetor_model_glm3)
 saveRDS(model_glm3, 'models/model_glm3.rds')
 
 # todas as variáveis com ***
-# acuracia 0.7216 kappa 0.0092 auc 0.7614
+# acuracia 0.7225 kappa 0.0092 auc 0.7601
 
 
 # Versão 4- Regressão Logística com nova composição de variáveis
@@ -152,82 +151,102 @@ print(vetor_model_glm4)
 saveRDS(model_glm4, 'models/model_glm4.rds')
 
 # todas as variáveis com ***, exceto os com **
-# acuracia 0.7205 kappa 0.0091 auc 0.7609
+# acuracia 0.72125 kappa 0.0091 auc 0.7595
 
 
-# Versão 5 - Random forest (a partir da formula_c = melhor resultado com Regressão Logística)
+# Versão 5 - Random forest (a partir da formula_b)
 
-model_rf1 <- rpart(formula_model_c, data = train_data, control = rpart.control(cp= .0005))
+model_rf1 <- rpart(formula_model_b, data = train_data, control = rpart.control(cp= .0005))
 summary(model_rf1)
 pred_rf1 <- predict(model_rf1, test_data, type = 'class')
 
 vetor_model_rf1 <- metricas(model_rf1, pred_rf1,'Modelo Random Forest 1')
 print(vetor_model_rf1)
 saveRDS(model_rf1, 'models/model_rf1.rds')
-# acuracia 0.8204 kappa 0.0193 auc 0.8577
+# acuracia 0.8192 kappa 0.0192 auc 0.8586
 
 
-# Versão 6- Random forest (a partir da formula_c, outros parâmetros)
+# Versão 6- Random forest (a partir da formula_c)
 
-model_rf2 <- rpart(formula_model_c, data = train_data, control = rpart.control(minsplit=50, cp= .005, maxdepth = 20))
+model_rf2 <- rpart(formula_model_c, data = train_data, control = rpart.control(cp= .0005))
 summary(model_rf2)
 pred_rf2 <- predict(model_rf2, test_data, type = 'class')
 
 vetor_model_rf2 <- metricas(model_rf2, pred_rf2,'Modelo Random Forest 2')
 print(vetor_model_rf2)
 saveRDS(model_rf2, 'models/model_rf2.rds')
-# acuracia 0.8204 kappa 0.0189 auc 0.8502
+# acuracia 0.8198 kappa 0.0193 auc 0.8589
 
 
+# Versão 7- Random forest (a partir da formula_c, outros parâmetros)
 
-# Versão 7 - XGBoost 1 (a partir da formula_c - mesmas variáveis)
+model_rf3 <- rpart(formula_model_c, data = train_data, control = rpart.control(minsplit=50, cp= .005, maxdepth = 20))
+summary(model_rf3)
+pred_rf3 <- predict(model_rf3, test_data, type = 'class')
 
-features_train <- as.matrix(train_data[,c("app", "diff_time_click", "channel", "count_click_by_ip", "device")])
-label_train <- as.matrix(train_data[,c("is_attributed")])
+vetor_model_rf3 <- metricas(model_rf3, pred_rf3,'Modelo Random Forest 3')
+print(vetor_model_rf3)
+saveRDS(model_rf3, 'models/model_rf3.rds')
+# acuracia 0.8197 kappa 0.0188 auc 0.8514
 
-features_test <- as.matrix(test_data[,c("app", "diff_time_click", "channel", "count_click_by_ip", "device")])
-label_test <- as.matrix(test_data[,c("is_attributed")])
 
-dtrain <- xgb.DMatrix(data = features_train, label = label_train) # convertendo em matriz densa
-dtest <- xgb.DMatrix(data = features_test, label = label_test)
+# Versão 8 - XGBoost 1 (a partir da formula_b - mesmas variáveis)
 
-model_xgboost1 <- xgboost(data = dtrain,
+features_train1 <- as.matrix(train_data[,c("app", "diff_time_click", "channel", "count_click_by_ip", "hour_click")])
+label_train1 <- as.matrix(train_data[,c("is_attributed")])
+
+features_test1 <- as.matrix(test_data[,c("app", "diff_time_click", "channel", "count_click_by_ip", "hour_click")])
+label_test1 <- as.matrix(test_data[,c("is_attributed")])
+
+dtrain1 <- xgb.DMatrix(data = features_train1, label = label_train1) # convertendo em matriz densa
+dtest1 <- xgb.DMatrix(data = features_test1, label = label_test1)
+
+model_xgboost1 <- xgboost(data = dtrain1,
                          max.depth = 2,
                          eta = 1,
                          nthread = 2,
                          nrounds = 2,
                          objective = "binary:logistic")
 
-prob_xgboost1 <- predict(model_xgboost1, dtest)
+prob_xgboost1 <- predict(model_xgboost1, dtest1)
 pred_xgboost1 <- as.numeric(prob_xgboost1 > 0.5)
 
-vetor_model_xgboost1 <- metricas(model_xgboost1, pred_xgboost1,'Modelo xGBoost')
+vetor_model_xgboost1 <- metricas(model_xgboost1, pred_xgboost1,'Modelo xGBoost 1')
 print(vetor_model_xgboost1)
 xgb.save(model_xgboost1, 'models/model_xgboost1')
 
-# acuracia 0.7795 kappa 0.0148 auc 0.8349
+# acuracia 0.7795 kappa 0.0148 auc 0.8356
 
 
-# Versão 8- XGBoost 2 (a partir da formula_c - mesmas variáveis, com variações nos parâmetros)
+# Versão 9- XGBoost 2 (a partir da formula_c - mesmas variáveis)
 
-model_xgboost2 <- xgboost(data = dtrain,
-                          max.depth = 3,
-                          eta = .9,
+features_train2 <- as.matrix(train_data[,c("app", "diff_time_click", "channel", "count_click_by_ip", "device")])
+label_train2 <- as.matrix(train_data[,c("is_attributed")])
+
+features_test2 <- as.matrix(test_data[,c("app", "diff_time_click", "channel", "count_click_by_ip", "device")])
+label_test2 <- as.matrix(test_data[,c("is_attributed")])
+
+dtrain2 <- xgb.DMatrix(data = features_train2, label = label_train2) # convertendo em matriz densa
+dtest2 <- xgb.DMatrix(data = features_test2, label = label_test2)
+
+model_xgboost2 <- xgboost(data = dtrain2,
+                          max.depth = 2,
+                          eta = 1,
                           nthread = 2,
                           nrounds = 2,
                           objective = "binary:logistic")
 
-prob_xgboost2 <- predict(model_xgboost2, dtest)
+prob_xgboost2 <- predict(model_xgboost2, dtest2)
 pred_xgboost2 <- as.numeric(prob_xgboost2 > 0.5)
 
 vetor_model_xgboost2 <- metricas(model_xgboost2, pred_xgboost2,'Modelo xGBoost 2')
 print(vetor_model_xgboost2)
 xgb.save(model_xgboost2, 'models/model_xgboost2')
 
-# acuracia 0.8186 kappa 0.0182 auc 0.8417
+# acuracia 0.7795 kappa 0.0148 auc 0.8356
 
 
-# Versão 9- XGBoost com Cross Validation
+# Versão 10- XGBoost com Cross Validation
 
 params <- list(booster = "gbtree",
                objective = "binary:logistic",
@@ -237,124 +256,47 @@ params <- list(booster = "gbtree",
                subsample = c(0.5, 0.6, 1),
                colsample_bytree=1
 )
-
-xgbcv <- xgb.cv(params = params, data = dtrain, nfold = 10, nrounds = 100,
+?xgb.cv
+xgbcv <- xgb.cv(params = params, data = dtrain2, nfold = 10, nrounds = 500,
                       showsd = T, stratified = T, print_every_n = 10, early_stop_round = 20,
-                      eval_metric = "auc", maximize = F)
+                      metrics = "auc", maximize = T)
 
-max_auc <- max(xgbcv$evaluation_log[, 2])
-xgbcv$evaluation_log[,2] == max_auc # 100
+xgbcv$evaluation_log
+
+max_auc <- max(xgbcv$evaluation_log[, 4]) # max test_auc_mean
+xgbcv$evaluation_log[,4] == max_auc # 500 -> iteração com maior auc
 
 
 # max_auc #100 - model training
-model_xgbcv <- xgb.train (params = params, data = dtrain, nrounds = 100,
-                   watchlist = list(val=dtest,train=dtrain),
-                   print_every_n = 20, early_stop_round = 20,
-                   maximize = F , eval_metric = "auc")
+model_xgbcv <- xgb.train (params = params, data = dtrain2, nrounds = 500,
+                   watchlist = list(val=dtest2,train=dtrain2),
+                   print_every_n = 100, early_stop_round = 20,
+                   maximize = T , metrics = "auc")
 
-prob_xgbcv <- predict (model_xgbcv,dtest)
+prob_xgbcv <- predict(model_xgbcv,dtest2)
 pred_xgbcv <- as.numeric(prob_xgbcv > 0.5)
 
 vetor_model_xgbcv <- metricas(model_xgbcv, pred_xgbcv, 'Modelo xGBoost CV')
 print(vetor_model_xgbcv)
 xgb.save(model_xgbcv, 'models/model_xgbcv')
 
-# acuracia 0.8222 kappa 0.02 auc 0.8688
-
-
-# Versão 10- XGBoost com Cross Validation 2
-
-params <- list(booster = "gbtree",
-               objective = "binary:logistic",
-               eta = c(0.5, 0.9, 1),
-               max_depth = c(2, 3),
-               min_child_weight = 1,
-               subsample = c(0.5, 0.6, 1),
-               colsample_bytree=1
-)
-
-xgbcv2 <- xgb.cv(params = params, data = dtrain, nfold = 10, nrounds = 200,
-                showsd = T, stratified = T, print_every_n = 10, early_stop_round = 20,
-                eval_metric = "auc", maximize = F)
-
-max_auc <- max(xgbcv2$evaluation_log[, 2])
-xgbcv2$evaluation_log[,2] == max_auc # 200
-
-
-# max_auc #200 - model training
-model_xgbcv2 <- xgb.train (params = params, data = dtrain, nrounds = 200,
-                          watchlist = list(val=dtest,train=dtrain),
-                          print_every_n = 20, early_stop_round = 20,
-                          maximize = F , eval_metric = "auc")
-
-prob_xgbcv2 <- predict (model_xgbcv2,dtest)
-pred_xgbcv2 <- as.numeric(prob_xgbcv2 > 0.5)
-
-vetor_model_xgbcv2 <- metricas(model_xgbcv2, pred_xgbcv2, 'Modelo xGBoost CV2')
-print(vetor_model_xgbcv2)
-xgb.save(model_xgbcv2, 'models/model_xgbcv2')
-# acuracia 0.8179 kappa 0.0194 auc 0.8655
-
-## incluir seed e rodar novamente
-
-
-# Versão 5 - SVM (não usar, pois o treinamento é muito lento)
-?svm
-model_svm <- svm(formula_model,
-                 data = train_data,
-                 scale = FALSE,
-                 type = 'C-classification',
-                 kernel = 'radial')
-pred_svm <- predict(model_svm, test_data)
-pred_result_svm <- ifelse(pred_svm > 0.5, 1, 0)
-
+# acuracia 0.7701 kappa 0.0147 auc 0.8469
 
 
 ## Comparando modelos
 compara_modelos <- rbind(vetor_model_glm1, vetor_model_glm2, vetor_model_glm3, vetor_model_glm4,
-                         vetor_model_rf, vetor_model_rf2, 
+                         vetor_model_rf1, vetor_model_rf2, vetor_model_rf3,
                          vetor_model_xgboost1, vetor_model_xgboost2, vetor_model_xgbcv)
-rownames(compara_modelos) <- c(1:9)
+rownames(compara_modelos) <- c(1:10)
 colnames(compara_modelos) <- c('Modelo', 'Acuracia', 'Kappa', 'AUC')
 compara_modelos <- as.data.frame(compara_modelos)
 print(compara_modelos)
 
-ggplot(compara_modelos, aes(x=Modelo, y=Acuracia, fill=Modelo)) + geom_bar(stat='identity')
-ggplot(compara_modelos, aes(x=Modelo, y=AUC, fill=Modelo)) + geom_bar(stat='identity')
+ggplot(compara_modelos, aes(x=Modelo, y=Acuracia, fill=Modelo)) + geom_bar(stat='identity') + theme(axis.text.x = element_text(angle=45,hjust=1,size=8))
+ggplot(compara_modelos, aes(x=Modelo, y=AUC, fill=Modelo)) + geom_bar(stat='identity')+ theme(axis.text.x = element_text(angle=45,hjust=.9,size=8))
 
-modelo_final <- 'avaliar melhor resultado'
 
-# vetor resultado MLII pg. 82
-# curva gráfico ROC MLI pg. 135
-# saveRDS pl 84 e xgbdump p. 282
-# testar tune()
-
-# Plot do modelo de melhor acurácia
-?prediction
-modelo_final <- model_xgbcv
-previsoes <- (predict(modelo_final, features_test, type = "class"))
-avaliacao <- (prediction(previsoes, test_data$is_attributed))
-class(previsoes)
-class(test_data$is_attributed)
-
-modelo_final <- model_xgbcv
-previsoes <- pred_xgbcv
-avaliacao <- prediction(previsoes, test_data$is_attributed) 
-
-# Função para Plot ROC
-plot_roc <- function(avaliacao, title){
-  perf <- performance(avaliacao, "tpr", "fpr")
-  plot(perf,col="black", lty=1, lwd=2,
-       main=title, cex.main=0.6, cex.lab=0.8, xaxs="i", yaxs="i")
-  abline(0,1, col="red")
-  auc <- performance(avaliacao, "auc")
-  auc <- unlist(slot(auc, "y.values"))
-  auc <- round(auc, 2)
-  legend(0.4,0.4,legend=c(paste0("AUC: ",auc)), cex=0.6, bty="n", box.col="white")
-}
-# Plot
-par(mfrow=c(1,2))
-plot_roc(avaliacao, title = "Curva ROC")
+# O modelo Random Forest 2 apresentou acurácia e curva AUC melhores, sendo o mais indicado para as previsões.
 
 
 
